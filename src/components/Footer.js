4ec6f6cf35aa766c8accc7dfc23020d6dbd30c54
Tobiasz <schettinitobia@gmail.com>
2025-02-25 14:01:20 +0100

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,70 @@
-// Footer.js
+// import React from 'react';
+// import { FaFacebook, FaInstagram, FaTripadvisor } from 'react-icons/fa';
+// import './Footer.css';
+// import { useTranslation } from 'react-i18next';
+
+// function Footer() {
+//   const { t } = useTranslation(); // Inizializza la traduzione
+//   return (
+//     <footer className="footer">
+//       <div className="footer-content">
+//         <p>{t('footer')}</p>
+//         <div className="social-links">
+//           <a href="https://www.facebook.com/p/Osteria-Caorlotta-Firenze-100088978474662/" target="_blank" rel="noopener noreferrer">
+//             <FaFacebook className="social-icon" />
+//           </a>
+//           <a href="https://www.instagram.com/osteriacaorlottafirenze/" target="_blank" rel="noopener noreferrer">
+//             <FaInstagram className="social-icon" />
+//           </a>
+//           <a href="https://www.tripadvisor.it/Restaurant_Review-g187895-d25226040-Reviews-Osteria_Caorlotta-Florence_Tuscany.html" target="_blank" rel="noopener noreferrer">
+//             <FaTripadvisor className="social-icon" />
+//           </a>
+//         </div>
+//       </div>
+//     </footer>
+//   );
+// }
+
+// export default Footer;
+
+
 import React from 'react';
 import { FaFacebook, FaInstagram, FaTripadvisor } from 'react-icons/fa';
 import './Footer.css';
 import { useTranslation } from 'react-i18next';
 
 function Footer() {
-  const { t } = useTranslation(); // Inizializza la traduzione
+  const { t } = useTranslation();
   return (
-    <footer className="footer">
-      <div className="footer-content">
+  <footer className="footer">
+    <div className="footer-content">
+      {/* Colonna sinistra */}
+      <div className="footer-left">
+        <p>Lungarno Amerigo Vespucci - 34r</p>
+      </div>
+
+      {/* Colonna centrale */}
+      <div className="footer-center">
         <p>{t('footer')}</p>
         <div className="social-links">
-          <a href="https://www.facebook.com/p/Osteria-Caorlotta-Firenze-100088978474662/" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.facebook.com/..." target="_blank" rel="noopener noreferrer">
             <FaFacebook className="social-icon" />
           </a>
-          <a href="https://www.instagram.com/osteriacaorlottafirenze/" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.instagram.com/..." target="_blank" rel="noopener noreferrer">
             <FaInstagram className="social-icon" />
           </a>
-          <a href="https://www.tripadvisor.it/Restaurant_Review-g187895-d25226040-Reviews-Osteria_Caorlotta-Florence_Tuscany.html" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.tripadvisor.it/..." target="_blank" rel="noopener noreferrer">
             <FaTripadvisor className="social-icon" />
           </a>
         </div>
       </div>
-    </footer>
+
+      {/* Colonna destra */}
+      <div className="footer-right">
+        <p>Credits: T. Schettini - T.N.T Media & Communication </p>
+      </div>
+    </div>
+  </footer>
   );
 }
 
