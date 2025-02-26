5ba87c8c9ba0452ff24f5f9e2d1048619289df84
Tobiasz <schettinitobia@gmail.com>
2025-02-26 16:23:55 +0100

    almost_done

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -15,10 +15,19 @@ function HomePage() {
     <div className="home-page">
       <img src={logoImage} alt="Scritta" className="homepage-logo" />
       {/* <img src={firenzeNero} alt="Firenze Nero" className="homepage-subtext" /> */}
+      <p className="homepage-subtitle">FIRENZE</p>
       <div className="home-buttons">
         <Link to="/prenotazione">
           <button className="btn btn-prenota">{t('prenota')}</button>
         </Link>
+      </div>
+      <div className="home-schedule">
+        <p>
+          Lungarno Amerigo Vespucci - 34r <br></br>
+          Venerdì - Sabato - Domenica: 12.30 - 13.45 | 19.00 - 21.45 <br></br>
+          Martedì - Mercoledì - Giovedì: 19.00 - 21.45<br></br>
+          📞 +39 345 169 1689 <br></br> 📧 osteriacaorlotta@gmail.com
+        </p>
       </div>
       {/* <img src={logoHome} alt="Logo" className="homepage-bottom-logo" /> */}
       {/* <img src={logoHome} alt="Logo" className="homepage-bottom-logo2" /> */}
