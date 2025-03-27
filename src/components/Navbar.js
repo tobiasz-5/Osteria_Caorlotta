8a75ab20446f7c136c6ba6ffa948cd6ad04b87c6
Tobiasz <schettinitobia@gmail.com>
2025-03-27 23:27:00 +0100

    lcp

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,6 +8,7 @@ import { useTranslation } from 'react-i18next';
 function Navbar() {
   const [isVisible, setIsVisible] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [showLogo, setShowLogo] = useState(false);
   const { t } = useTranslation();
 
   const toggleMenu = () => {
@@ -20,6 +21,7 @@ function Navbar() {
     setIsVisible(false);
   };
 
+  // Mostra la navbar al movimento del mouse (desktop)
   useEffect(() => {
     const handleMouseMove = (event) => {
       if (event.clientY < 240) {
@@ -35,12 +37,37 @@ function Navbar() {
     };
   }, [isMenuOpen]);
 
+  // Ritarda la comparsa del logo per evitare che diventi LCP
+  useEffect(() => {
+    const showOnIntent = () => setShowLogo(true);
+
+    // Timeout di fallback dopo 2.5s
+    const timer = setTimeout(showOnIntent, 2500);
+
+    // Mostra il logo al primo movimento/tocco
+    window.addEventListener('mousemove', showOnIntent, { once: true });
+    window.addEventListener('touchstart', showOnIntent, { once: true });
+
+    return () => {
+      clearTimeout(timer);
+      window.removeEventListener('mousemove', showOnIntent);
+      window.removeEventListener('touchstart', showOnIntent);
+    };
+  }, []);
+
   return (
     <nav className={`navbar ${isVisible ? 'visible' : ''}`}>
       <div className="navbar-content">
         <div className="navbar-logo">
           <Link to="/">
-            <img src={logo} alt="Il Logo Caorlotta" loading='eager' fetchPriority='high'/>
+            {showLogo && (
+              <img
+                src={logo}
+                alt="Il Logo Caorlotta"
+                loading="lazy"
+                fetchpriority="low"
+              />
+            )}
           </Link>
         </div>
         <button
@@ -53,27 +80,13 @@ function Navbar() {
           <span className="menu-icon"></span>
         </button>
         <ul id="navbar-links" className={`navbar-links ${isMenuOpen ? 'open' : ''}`}>
-          <li>
-            <Link to="/" onClick={closeMenu}>Home</Link>
-          </li>
-          <li>
-            <Link to="/prenotazione" onClick={closeMenu}>{t('navbar.prenotazione')}</Link>
-          </li>
-          <li>
-            <Link to="/chi-siamo" onClick={closeMenu}>{t('navbar.chi_siamo')}</Link>
-          </li>
-          <li>
-            <Link to="/menu" onClick={closeMenu}>{t('navbar.menu')}</Link>
-          </li>
-          <li>
-            <Link to="/dove-siamo" onClick={closeMenu}>{t('navbar.dove_siamo')}</Link>
-          </li>
-          <li>
-            <Link to="/recensioni" onClick={closeMenu}>{t('navbar.recensioni')}</Link>
-          </li>
-          <li>
-            <Link to="/photogallery" onClick={closeMenu}>{t('navbar.photo_gallery')}</Link>
-          </li>
+          <li><Link to="/" onClick={closeMenu}>Home</Link></li>
+          <li><Link to="/prenotazione" onClick={closeMenu}>{t('navbar.prenotazione')}</Link></li>
+          <li><Link to="/chi-siamo" onClick={closeMenu}>{t('navbar.chi_siamo')}</Link></li>
+          <li><Link to="/menu" onClick={closeMenu}>{t('navbar.menu')}</Link></li>
+          <li><Link to="/dove-siamo" onClick={closeMenu}>{t('navbar.dove_siamo')}</Link></li>
+          <li><Link to="/recensioni" onClick={closeMenu}>{t('navbar.recensioni')}</Link></li>
+          <li><Link to="/photogallery" onClick={closeMenu}>{t('navbar.photo_gallery')}</Link></li>
         </ul>
       </div>
       {isMenuOpen && <div className="overlay" onClick={closeMenu}></div>}
