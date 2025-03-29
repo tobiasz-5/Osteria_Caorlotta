ba5eeb4d7b748926959c609f1b42757784a618ba
Tobiasz <schettinitobia@gmail.com>
2025-03-29 11:48:31 +0100

    opt. bundle

--- src/components/Photogallery.js
+++ src/components/Photogallery.js
@@ -3,45 +3,20 @@ import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
 import './Photogallery.css';
 
-import piatto1 from '../assets/images/photogallery/piatto1.jpg';
-import piatto2 from '../assets/images/photogallery/piatto2.jpg';
-import piatto3 from '../assets/images/photogallery/piatto3.jpg';
-import piatto4 from '../assets/images/photogallery/piatto4.jpg';
-import piatto5 from '../assets/images/photogallery/piatto5.jpg';
-import piatto6 from '../assets/images/photogallery/piatto6.jpg';
-import piatto7 from '../assets/images/photogallery/piatto7.jpg';
-import piatto8 from '../assets/images/photogallery/piatto8.jpg';
-import piatto9 from '../assets/images/photogallery/piatto9.jpg';
-import piatto10 from '../assets/images/photogallery/piatto10.jpg';
-import piatto11 from '../assets/images/photogallery/piatto11.jpg';
-import piatto12 from '../assets/images/photogallery/piatto12.jpg';
-import piatto13 from '../assets/images/photogallery/piatto13.jpg';
-import piatto14 from '../assets/images/photogallery/piatto14.jpg';
-import piatto15 from '../assets/images/photogallery/piatto15.jpg';
-import piatto16 from '../assets/images/photogallery/piatto16.jpg';
-import piatto17 from '../assets/images/photogallery/piatto17.jpg';
-import piatto18 from '../assets/images/photogallery/piatto18.jpg';
-import piatto19 from '../assets/images/photogallery/piatto19.jpg';
-import piatto20 from '../assets/images/photogallery/piatto20.jpg';
-import piatto21 from '../assets/images/photogallery/piatto21.jpg';
-import piatto22 from '../assets/images/photogallery/piatto22.jpg';
-import piatto23 from '../assets/images/photogallery/piatto23.jpg';
-import piatto24 from '../assets/images/photogallery/piatto24.jpg';
-import piatto25 from '../assets/images/photogallery/piatto25.jpg';
-import piatto26 from '../assets/images/photogallery/piatto26.jpg';
-import piatto27 from '../assets/images/photogallery/piatto27.jpg';
-import piatto28 from '../assets/images/photogallery/piatto28.jpg';
-import piatto29 from '../assets/images/photogallery/piatto29.jpg';
-import piatto30 from '../assets/images/photogallery/piatto30.jpg';
+// Import dinamico delle immagini dalla cartella photogallery
+const images = importAll(
+  require.context('../assets/images/photogallery', false, /\.(jpe?g|png|webp)$/)
+);
+
+function importAll(r) {
+  return r
+    .keys()
+    .sort() // Ordina i nomi dei file (es: ./piatto1.jpg, ./piatto2.jpg, ...)
+    .map(r); // Importa le immagini in quell'ordine
+}
 
 function Photogallery() {
   const { t } = useTranslation();
-  const images = [
-    piatto1, piatto2, piatto3, piatto4, piatto5, piatto6, piatto7, piatto8,
-    piatto9, piatto10, piatto11, piatto12, piatto13, piatto14, piatto15, piatto16,
-    piatto17, piatto18, piatto19, piatto20, piatto21, piatto22, piatto23, piatto24,
-    piatto25, piatto26, piatto27, piatto28, piatto29, piatto30
-  ];
   const galleryRef = useRef(null);
   const [isMobile, setIsMobile] = useState(false);
 
@@ -113,6 +88,7 @@ function Photogallery() {
           `}
         </noscript>
       </Helmet>
+
       <div className="photogallery-container">
         <div className="additional-layer"></div>
         <div className="additional-layer2"></div>
